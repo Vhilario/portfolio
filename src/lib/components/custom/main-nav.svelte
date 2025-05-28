8065e3a1f298adeb41ce5419ba36fe3e9eec86e7
Vincent Hilario <84406330+Vhilario@users.noreply.github.com>
2025-05-28 11:25:22 -0500

    Initial commit

---
+++ src/lib/components/custom/main-nav.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import { page } from "$app/stores";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        const path = $page.url.pathname;
+        console.log(`Current path: ${path}`);
+    });
+</script>
+
+<div class="flex flex-row justify-between items-center h-12 border-b border-gray-200 px-2">
+    <h1 class="text-xl font-bold">
+        <a href="/">Vincent Hilario</a>
+    </h1>
+    <Button class="w-24" href="/blog">
+        My Blog
+    </Button>
+</div>
+
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+	import MainNav from '$lib/components/custom/main-nav.svelte';
+	let { children } = $props();
+</script>
+
+<div class="flex flex-col h-screen">
+	<!-- MainNav always at the top -->
+	<!--<MainNav /> -->
+	<!-- Content below, scrollable if needed -->
+	<div class="flex-1 min-h-0 overflow-y-auto bg-blue-500">
+		{@render children()}
+	</div>
+</div>
