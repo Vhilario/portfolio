7dbf99d072d89fd5a8634bf9d6c4106ae29f4d59
Vincent Hilario <84406330+Vhilario@users.noreply.github.com>
2025-07-15 17:03:15 -0500

    Revamp AboutMe Section
    Updated the carousel items, filling with the proper information. Still a WIP, this is really just an EOD commit.

--- src/lib/components/sections/AboutMe.svelte
+++ src/lib/components/sections/AboutMe.svelte
@@ -121,41 +121,70 @@
                                     <Carousel.Item>
                                         <div class="flex flex-row gap-4">
                                             <AspectRatio ratio={16 / 9} class="flex items-center justify-center">
-                                                <img src="https://placehold.co/1600x900" alt="Vincent Hilario" class="w-full h-full object-cover">
+                                                <img src="/pictureofme.jpg" alt="Vincent Hilario" class="h-full object-cover rounded-lg">
+                                            </AspectRatio>
+                                            <div class="flex flex-col gap-4">
+                                                <p>
+                                                    I'm <span class="font-bold">Vincent Hilario</span>, a software developer from Tyler, Texas. Well, technically I'm a software tester, but I want to be a software developer someday.
+                                                </p>
+                                                <p>
+                                                    I grew up in Quitman, Texas, before moving to Tyler in 2015, where I've lived ever since. I went to school at <span class='text-[#CB6015] font-bold'>The University of Texas at Tyler</span>, where I graduated in 2023 with a <span class='font-bold'>Bachelor of Science in Computer Science</span>.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <div class="flex flex-col gap-4 py-2">
+                                            <h1 class="text-2xl font-bold text-center">My Story:</h1>
+                                            <p class="px-4 text-md">
+                                                I discovered my passion for software development in high school when I took a computer science class. I was immediately hooked and have been coding ever since.
+                                                Coding spoke to me because it was the first subject in school that I felt challenged me. At the risk of sounding boastful, other subjects came very easy to me, but coding was the one that I had to work for.
+                                            </p>
+                                            <p class="px-4 text-md">    
+                                                I'm currently working as a <span class="font-bold">Software Test Engineer</span> at <span class="text-[#6400FF] font-bold">Trane Technologies</span>. My current goal is to get my first job as a software developer, and I'm working towards that goal by learning new technologies and building my portfolio.
+                                            </p>
+
+                                        </div>
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <div class="flex flex-row gap-4">
+                                            <AspectRatio ratio={16 / 9} class="flex items-center justify-center">
+                                                <img src="/css_meme.png" alt="Unfortunately Timed Frontend Failure" class="w-full h-full object-cover">
                                             </AspectRatio>
                                             <p class="w-1/2">
-                                                I'm <span class="font-bold">Vincent Hilario</span>, a software developer from Tyler, Texas. Well, technically I'm a software tester, but I want to be a software developer someday.
+                                                I'd say I prefer <span class="font-bold">frontend</span> development, but I'm not afraid to get my hands dirty with the <span class="font-bold">backend</span>. I've developed a few <span class="font-bold">full-stack applications</span>, and I consider myself to be very versatile.
                                             </p>
                                         </div>
                                     </Carousel.Item>
                                     <Carousel.Item>
                                         <div class="flex flex-row gap-4">
                                             <AspectRatio ratio={16 / 9} class="flex items-center justify-center">
-                                                <img src="https://placehold.co/600x400" alt="Vincent Hilario" class="w-full h-full object-cover">
+                                                <video src="/biking.mp4" autoplay loop muted class="w-full h-full object-cover rounded-lg">
+                                                </video>
                                             </AspectRatio>
-                                            <p class="w-1/2">
-                                                I'm looking for that first job to give me my break into the software engineering industry.
-                                            </p>
+                                            <div class="flex flex-col gap-4">
+                                                <p>
+                                                    Other hobbies of mine include playing video games, biking, and well, coding.
+                                                </p>
+                                                <p>
+                                                    I've been on something of a health kick lately, and and I discovered that I really like <span class="font-bold">biking</span>. It's good exercise, and way more satisfying than running, because you get to travel further, and generally see more sights.
+                                                </p>
+                                                <p>
+                                                    I'm also a big fan of <span class="font-bold">video games</span>. I've been playing them since I was a kid, and I still love them to this day.
+                                                </p>
+                                            </div>
                                         </div>
                                     </Carousel.Item>
                                     <Carousel.Item>
-                                        <div class="flex flex-row gap-4">
-                                            <AspectRatio ratio={16 / 9} class="flex items-center justify-center">
-                                                <img src="https://placehold.co/600x400" alt="Vincent Hilario" class="w-full h-full object-cover">
-                                            </AspectRatio>
-                                            <p class="w-1/2">
-                                                I'd say I prefer frontend development, but I'm not afraid to get my hands dirty with the backend.
-                                            </p>
-                                        </div>
-                                    </Carousel.Item>
-                                    <Carousel.Item>
-                                        <div class="flex flex-row gap-4">
-                                            <AspectRatio ratio={16 / 9} class="flex items-center justify-center">
-                                                <img src="https://placehold.co/600x400" alt="Vincent Hilario" class="w-full h-full object-cover">
-                                            </AspectRatio>
-                                            <p class="w-1/2">
-                                                Other hobbies of mine include playing video games, biking, and well, coding.
-                                            </p>
+                                        <div class="flex flex-col gap-4">
+                                            <h1 class="text-2xl font-bold text-center">Fun Facts About Me:</h1>
+                                            <ul class="list-disc list-inside font-bold space-y-3">
+                                                <li>Favorite Color: <span class="bg-[#004225] text-white px-2 py-1 rounded-md">British Racing Green</span></li>
+                                                <li>Favorite Album: <span class="bg-[#ea3f23] text-white px-2 py-1 rounded-md">Rush: 2112</span></li>
+                                                <li>Favorite Video Game: <span class="bg-gray-400 text-white px-2 py-1 rounded-md">Battlefield 4</span></li>
+                                                <li>Favorite Movie: <span class="bg-[#82cad6] text-red-500 px-2 py-1 rounded-md">Nacho Libre</span></li>
+                                                <li>I went to the same High School as: <span class="bg-red-500 text-yellow-400 px-2 py-1 rounded-md">Patrick Mahomes</span></li>
+                                            </ul>
                                         </div>
                                     </Carousel.Item>
                                 </Carousel.Content>
